b92366d7b2b224d271ec428f10c2b9438d7827f7
Justin Payne <justinthecore@gmail.com>
2022-10-30 23:23:51 -0700

    Added edit and delete controllers + modals for edit and delete but functionality not working yet

---
+++ CashTrak/Controllers/DeleteController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CashTrak.Models;
+
+namespace CashTrak.Controllers
+{
+    public class DeleteController : Controller
+    {
+        private CashRequestDbContext MyContext { get; set; }
+
+/*        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var cashRequest = MyContext.CashRequests.Find(id);
+            return View(cashRequest);
+        }*/
+                
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var cashRequest = MyContext.CashRequests.Find(id);
+            MyContext.CashRequests.Remove(cashRequest);
+            MyContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CashTrak/Controllers/EditController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CashTrak.Models;
+
+namespace CashTrak.Controllers
+{
+    public class EditController : Controller
+    {
+        private CashRequestDbContext MyContext { get; set; }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var cashRequest = MyContext.CashRequests.Find(id);
+            return View(cashRequest);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CashRequest cashRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                if (cashRequest.CashRequestID == 0)
+                {
+                    MyContext.CashRequests.Add(cashRequest);
+                }
+                else
+                {
+                    MyContext.CashRequests.Update(cashRequest);
+                }
+                MyContext.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (cashRequest.CashRequestID == 0) ? "Add" : "Edit";
+                return View(cashRequest);
+            }
+        }
+    }
+}
