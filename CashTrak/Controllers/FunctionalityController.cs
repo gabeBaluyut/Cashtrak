fcf4dbf52dd804f0c21b437c594ff4b99e130e01
Gabriel Baluyut <60728482+gabeBaluyut@users.noreply.github.com>
2022-11-01 10:58:30 -0700

    Created controller for creating requests, added HTML inputs

---
+++ CashTrak/Controllers/FunctionalityController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CashTrak.Models;
+
+namespace CashTrak.Controllers
+{
+    public class FunctionalityController : Controller
+    {
+
+        private CashRequestDbContext MyContext { get; set; }
+
+        public FunctionalityController(CashRequestDbContext context)
+        {
+            MyContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Add", new CashRequest());
+        }
+
+        [HttpPost]
+        public IActionResult Create(CashRequest cashRequest)
+        {
+            MyContext.CashRequests.Add(cashRequest);
+            MyContext.SaveChanges();
+            return RedirectToAction("index", "home");
+        }
+    }
+}
